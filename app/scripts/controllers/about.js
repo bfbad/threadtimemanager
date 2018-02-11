ffaf4e3bb26a014f6e026214fbf32915a1049a71
Brandon Ferguson <brandonferguson@Brandons-MacBook-Pro.local>
2018-02-10 21:31:45 -0500

    Create angular app with Yeoman

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name threadApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the threadApp
+ */
+angular.module('threadApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name threadApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the threadApp
+ */
+angular.module('threadApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
